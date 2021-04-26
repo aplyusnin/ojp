3292f00bfe0227c3e97c93b5e53955e94c838512
unknown <a.plyusnin@g.nsu.ru>
2021-04-26 19:52:46 +0700

    fix

--- Task_5/src/main/java/ru/nsu/fit/ojp/plyusnin/Task_5/Main.java
+++ Task_5/src/main/java/ru/nsu/fit/ojp/plyusnin/Task_5/Main.java
@@ -1,16 +1,38 @@
 package ru.nsu.fit.ojp.plyusnin.Task_5;
 
-public class Main {
 
+
+public class Main {
+	private static class MyBarrier {
+		private int permits;
+		private int current;
+		public MyBarrier(int permits){
+			this.permits = permits;
+			this.current = permits;
+		}
+		public synchronized void acquire() throws InterruptedException{
+			if (current == 1){
+				current = permits;
+				notifyAll();
+			}
+			else{
+				current--;
+				wait();
+			}
+		}
+
+
+	}
 	public static void main(String[] args){
 		Object o1 = new Object();
 		Object o2 = new Object();
+		MyBarrier barrier = new MyBarrier(2);
 
 		synchronized (o1) {
 			new Thread(() -> {
 				try {
 					synchronized (o2) {
-						Thread.sleep(1000);
+						barrier.acquire();
 						synchronized (o1) {
 							System.out.print("Hi from thead");
 						}
@@ -19,7 +41,7 @@ public class Main {
 				catch (InterruptedException ignored) {}
 			}).start();
 			try{
-				Thread.sleep(100);
+				barrier.acquire();
 			}
 			catch (InterruptedException ignored){}
 			synchronized (o2){
