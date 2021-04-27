c113313701ef569c3e8bfd72ed751e6aaa5efb9c
unknown <a.plyusnin@g.nsu.ru>
2021-04-27 12:53:31 +0700

    fix

--- Task_6/src/cpuinfo/cpp/cpuinfo.cpp
+++ Task_6/src/cpuinfo/cpp/cpuinfo.cpp
@@ -10,25 +10,25 @@ JNIEXPORT jobject JNICALL Java_ru_nsu_fit_ojp_plyusnin_task_16_MyJNIClass_getCpu
     in.open("/proc/cpuinfo", ios::in);
 	if (!in.is_open()) return nullptr;
 	
-	jclass cls = (*env)->FindClass(env, "java/lang/HashMap");
+	jclass cls = env->FindClass(env, "java/lang/HashMap");
 	if (cls == nullptr) return nullptr;
-	jmethodId init = (*env)->GetMethodID(env, cls, "<init>", "()V");
+	jmethodId init = env->GetMethodID(env, cls, "<init>", "()V");
 	if (init == nullptr) return nullptr;
 	
-	jobject cur = (*env)->newObject(env, cls, init);
+	jobject cur = env->newObject(env, cls, init);
 	if (cur == nullptr) return nullptr;
-	jmethodId put = (*env)->GetMethodID(env, cls, "put", "(Ljava/lang/Object;Ljava/lang/String)Ljava/lang/String")
+	jmethodId put = env->GetMethodID(env, cls, "put", "(Ljava/lang/Object;Ljava/lang/String)Ljava/lang/String")
 	str::string line;
-	while (getline(line, in)){
+	while (getline(in, line)){
 		int p;
 		for (p = 0; line[p] != ':'; p++);
 		std::string key = line.substr(0, p);
 		std::string val = line.substr(p + 1, (int)line.size() - p - 1);
-		jstring jkey = (*env)->NewStringUTF(env, key.c_str());
-		jstring jval = (*env)->NewStringUTF(env, val.c_str());
+		jstring jkey = env->NewStringUTF(env, key.c_str());
+		jstring jval = env->NewStringUTF(env, val.c_str());
 		if (jkey == nullptr || jval == nullptr) continue;
-		(*env)->CallObjectMethod(env, cur, put, jkey, jval);
+		env->CallObjectMethod(env, cur, put, jkey, jval);
 	}
 	
-	return (*env)->NewGlobalRef(env, cur);
+	return env->NewGlobalRef(env, cur);
 }
